9fd9369c57fd6d8a3847a2fcd25d2195a1b13806
Benício Barreto <benicio.daniel@gmail.com>
2020-10-12 13:02:35 -0300

    Wrote the landing basic text

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,26 @@ import React from "react";
 import "./styles/global.css";
 import "./styles/pages/landing.css";
 
+import logoImg from "./images/logo.svg";
+
 const App: React.FC = () => {
   return (
     <div id="page-landing">
-      <h1>global</h1>
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Logo" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Curitiba</strong>
+          <span>Paraná</span>
+        </div>
+
+        <a href="" className="enter-app">></a>
+      </div>
     </div>
   );
 };
